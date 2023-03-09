f1630ca7f0c2a8f86f82c8478cf6e4a087de5438
sunshovo <sunsh97@khu.ac.kr>
2023-03-09 21:47:21 +0900

    second commit

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-name = input("What is your name? ")
-age = input("How old are you? ")
-print("Hello, " + name + "! Nice to meet you. You are " + age + " years old.")
+name = input("What is your name?")
+age = input("How old are you?")
+city = input("Where do you live?")
+print("Hello, " + name + "! Nice to meet you. You are " + age + " years old and you live in " + city + ".")
